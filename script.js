2ded4b4abc2aa2d4e2aaa70e82a7deecb8a938e0
Cory <corymclean0@gmail.com>
2023-08-25 16:44:45 -0400

    DONE

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+const submit=document.getElementById('submit')
+submit.addEventListener('click',function(){alert('Thanks for applying... Cory will be in touch ...maybe ...idk.   Ciao!')
+    
+})
